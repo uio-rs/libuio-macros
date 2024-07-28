242b9e31bf38122236446c9ada25d7d19a27ae65
Christian Saide <me@csaide.dev>
2024-07-28 14:10:42 -0400

    feat: Added syn/quote dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_attribute]
 pub fn main(attr: TokenStream, item: TokenStream) -> TokenStream {
-    println!("attr: \"{attr}\"");
-    println!("item: \"{item}\"");
-    item
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(item as DeriveInput);
+
+    // Generate new Rust code based on the transformed AST
+    let expanded = quote! {
+        #input
+    };
+
+    // Return the generated code
+    let gen = TokenStream::from(expanded);
+    println!("generated code: {:?}", gen);
+    gen
 }
