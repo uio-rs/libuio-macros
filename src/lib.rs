fedcfddeb6c4844ba609df421566314006ad5ead
Christian Saide <me@csaide.dev>
2024-07-28 14:33:27 -0400

    feat: Added full handling of a basic libuio::main

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,41 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, ItemFn, ReturnType};
 
 #[proc_macro_attribute]
-pub fn main(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
-    let input = parse_macro_input!(item as DeriveInput);
+    let input = parse_macro_input!(item as ItemFn);
+
+    // Check if the function is async
+    if input.sig.asyncness.is_none() {
+        panic!("The function must be async");
+    }
+
+    // Check if the function returns ()
+    if let ReturnType::Type(_, _) = &input.sig.output {
+        panic!("The function must return ()");
+    }
 
     // Generate new Rust code based on the transformed AST
     let expanded = quote! {
-        #input
+        async fn __internal_main() {
+            #input.block
+        }
+
+        fn main() {
+            // First we need to create a new thread pool to execute on.
+            let pool = libuio::executor::ThreadPoolBuilder::new()
+                .name_prefix("executor")
+                .create()
+                .expect("Failed to configure thread pool.");
+
+            // Now we spawn our main async task, which will drive any/all async operations needed by our
+            // application.
+            pool.spawn_ok(__internal_main());
+
+            pool.wait();
+        }
     };
 
     // Return the generated code
