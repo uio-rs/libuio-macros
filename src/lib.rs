cacf3a3565b15d37125a30de1b1a015a2c246a3f
Christian Saide <me@csaide.dev>
2024-07-28 15:37:30 -0400

    bug: Handle any/all return types and ensure we have some comments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,37 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn, ReturnType};
+use syn::{parse_macro_input, ItemFn};
 
 #[proc_macro_attribute]
 pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
     let input = parse_macro_input!(item as ItemFn);
     let block = input.block;
+    let output = input.sig.output;
 
     // Check if the function is async
     if input.sig.asyncness.is_none() {
         panic!("The function must be async");
     }
 
-    // Check if the function returns ()
-    if let ReturnType::Type(_, _) = &input.sig.output {
-        panic!("The function must return ()");
-    }
-
     // Generate new Rust code based on the transformed AST
     let expanded = quote! {
-        async fn __internal_main() #block
+        // Create new async function so we can replace the main function. Ensuring we handle the
+        // output type and proper block representation.
+        async fn __internal_main() #output #block
 
-        fn main() {
+        // Create the new main function with the proper output type to satisfy the original main
+        // function.
+        fn main() #output {
             // First we need to create a new thread pool to execute on.
-            let pool = libuio::executor::ThreadPoolBuilder::new()
+            libuio::executor::ThreadPoolBuilder::new()
                 .name_prefix("libuio-executor")
                 .create()
                 .expect("Failed to configure thread pool.");
 
             // Now we spawn our main async task, which will drive any/all async operations needed by our
             // application.
-            libuio::executor::block_on(__internal_main());
+            libuio::executor::block_on(__internal_main())
         }
     };
 
